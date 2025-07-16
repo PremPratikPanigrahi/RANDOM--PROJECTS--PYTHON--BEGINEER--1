be122caf4d279320efaadb44d8890f322a571942
PremPratikPanigrahi <prempratik.318@gmail.com>
2025-07-16 17:50:58 +0530

    Create hello_world_through_guess.py

---
+++ hello_world_through_guess.py
@@ -0,0 +1,130 @@
+import random
+import string
+import time
+
+while "a":
+ letters = string.ascii_letters
+ digits = string.digits         
+ symb = string.punctuation   
+ r = letters + digits + symb
+ rt = random.choice(r)
+ time.sleep(0.02)
+ print(rt)
+ if rt=="H":break
+
+while "a":
+ letters = string.ascii_letters
+ digits = string.digits         
+ symb = string.punctuation   
+ r = letters + digits + symb
+ rt = random.choice(r)
+ time.sleep(0.02)
+ print("H"+rt)
+ if rt=="e":break
+
+while "a":
+ letters = string.ascii_letters
+ digits = string.digits         
+ symb = string.punctuation   
+ r = letters + digits + symb
+ rt = random.choice(r)
+ time.sleep(0.02)
+ print("He"+rt)
+ if rt=="l":break
+
+while "a":
+ letters = string.ascii_letters
+ digits = string.digits         
+ symb = string.punctuation   
+ r = letters + digits + symb
+ rt = random.choice(r)
+ time.sleep(0.02)
+ print("Hel"+rt)
+ if rt=="l":break
+
+while "a":
+ letters = string.ascii_letters
+ digits = string.digits         
+ symb = string.punctuation   
+ r = letters + digits + symb
+ rt = random.choice(r)
+ time.sleep(0.02)
+ print("Hell"+rt)
+ if rt=="o":break
+
+while "a":
+ letters = string.ascii_letters
+ digits = string.digits         
+ symb = string.punctuation
+ space=" "   
+ r = letters + digits + symb + space
+ rt = random.choice(r)
+ time.sleep(0.02)
+ print("Hello"+rt)
+ if rt==" ":break
+
+while "a":
+ letters = string.ascii_letters
+ digits = string.digits         
+ symb = string.punctuation
+ space=" "   
+ r = letters + digits + symb + space
+ rt = random.choice(r)
+ time.sleep(0.02)
+ print("Hello "+rt)
+ if rt=="W":break
+
+while "a":
+ letters = string.ascii_letters
+ digits = string.digits         
+ symb = string.punctuation
+ space=" "   
+ r = letters + digits + symb + space
+ rt = random.choice(r)
+ time.sleep(0.02)
+ print("Hello W"+rt)
+ if rt=="o":break
+
+while "a":
+ letters = string.ascii_letters
+ digits = string.digits         
+ symb = string.punctuation
+ space=" "   
+ r = letters + digits + symb + space
+ rt = random.choice(r)
+ time.sleep(0.02)
+ print("Hello Wo"+rt)
+ if rt=="r":break
+
+while "a":
+ letters = string.ascii_letters
+ digits = string.digits         
+ symb = string.punctuation
+ space=" "   
+ r = letters + digits + symb + space
+ rt = random.choice(r)
+ time.sleep(0.02)
+ print("Hello Wor"+rt)
+ if rt=="l":break
+
+while "a":
+ letters = string.ascii_letters
+ digits = string.digits         
+ symb = string.punctuation
+ space=" "   
+ r = letters + digits + symb + space
+ rt = random.choice(r)
+ time.sleep(0.02)
+ print("Hello Worl"+rt)
+ if rt=="d":break
+
+while "a":
+ letters = string.ascii_letters
+ digits = string.digits         
+ symb = string.punctuation
+ space=" "   
+ r = letters + digits + symb + space
+ rt = random.choice(r)
+ time.sleep(0.02)
+ print("Hello World"+rt)
+ if rt=="!":break
